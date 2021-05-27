8b78884bb5677d32f96fb7e3e6b277ad161eb752
AnahitaZ-prog <84908368+AnahitaZ-prog@users.noreply.github.com>
2021-05-27 15:11:10 +0430

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,11 @@
+print("Yek adad varid konid:")
+first_number=input()
+first_number_float=float(first_number)
+
+print("yek adade digar vared konid:")
+second_number=input()
+second_number_float=float(second_number)
+
+result=first_number_float+second_number_float
+
+print(result)
