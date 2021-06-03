7b2dc92b193e6b5a92e562011d268df4283dbaa0
AnahitaZ-prog <84908368+AnahitaZ-prog@users.noreply.github.com>
2021-06-03 15:58:46 +0430

    Add files via upload

---
+++ tamrin.py
@@ -0,0 +1,7 @@
+
+def mostatil(tool,arz):
+    mohit=2*(tool+arz)
+    print("mohih:",mohit)
+vorodi_1=int(input("tool adad vared konid;\n"))
+vorodi_2=int(input("arz adad vared konid;\n"))
+mostatil(vorodi_1,vorodi_2)
