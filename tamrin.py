56c0872a021b7558cdada2d439009f4f5a591ab3
AnahitaZ-prog <84908368+AnahitaZ-prog@users.noreply.github.com>
2021-06-03 17:22:45 +0430

    Update tamrin.py

--- tamrin.py
+++ tamrin.py
@@ -1,13 +1,8 @@
+def math(number):
+    num_1 = number + 1
+    return num_1
+    print(num_1)
+vorodi = int(input("Enter a number\n"))
+math(vorodi)   
 
-def mostatil(tool , arz , command):
-    if command == "mohit":
-        mohit = 2*(tool+arz)
-        print("mohit:\t" , mohit)
-    if command == "masahat":
-        masahat = tool * arz
-        print("masahat:\t", masahat)
-vorodi_1 = int(input("tool adad vared konid;\n"))
-vorodi_2 = int(input("arz adad vared konid;\n"))
-vorodi_3 = input("Command ra vared konid:\n")
-mostatil(vorodi_1 , vorodi_2 , vorodi_3)
 
