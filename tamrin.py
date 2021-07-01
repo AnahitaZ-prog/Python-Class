85486c6fb5bbb31dd513cd61be87ff4a838e8a6d
AnahitaZ-prog <84908368+AnahitaZ-prog@users.noreply.github.com>
2021-07-01 15:49:41 +0430

    Update tamrin.py

--- tamrin.py
+++ tamrin.py
@@ -1,17 +1,8 @@
-def mostatil(tool , arz , command):
-     if command == "mohit":
-         mohit = 2*(tool+arz)
-         result= mohit
-     if command == "masahat":
-         masahat = tool * arz
-         result= masahat
-     return result
+class Cat:
+    name = "Rex"
+    age = 2
 
-vorodi_1 = int(input("tool adad vared konid;\n"))
-vorodi_2 = int(input("arz adad vared konid;\n"))
-vorodi_3 = input("Command ra vared konid:\n")
-result = mostatil(vorodi_1 , vorodi_2 , vorodi_3)
-print(result)
-mostatil(vorodi_1 , vorodi_2 , vorodi_3) 
+    def hello():
+        print("Mio Mio")
 
-
+Cat.hello()            
